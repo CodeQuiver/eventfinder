97ec5c57226fb4eaa32fe7861056d798f05148ff
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-11-07 16:24:49 -0500

    set up folder structure for components and pages, began work on Event component

---
+++ client/src/components/otherComponents/event.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+//========= EVENT ==========//
+class Event extends React.Component {
+    // contains event id
+    // outlines div where a single event result prints
+    // renders "EventDetails"
+    
+    
+    render() {
+        <EventDetails />
+    }
+}
+//========= END EVENT ==========//
+
+
+
+//========= EVENT DETAILS ==========//
+class EventDetails extends React.Component {
+// contains details for an event
+
+
+}
+//========= END EVENT DETAILS ==========//
+
+
+
+//========= WEATHER =========//
+class Weather extends React.Component {
+    //contains weather data for an event
+    
+}
+//========= END WEATHER =========//
+
+
+
+
+
+
+
+export default Event;
